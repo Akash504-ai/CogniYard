53c3bb99815948a785c37507ea413eb7a0779230
Akash Santra <santraakash999@gmail.com>
2026-08-25 21:19:07 +0530

    Delete server directory

--- server/routes/aiRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const aiController = require('../controllers/aiController');
-const { protect } = require('../middleware/auth');
-
-router.post('/chat', protect, aiController.chat);
-
-module.exports = router;
